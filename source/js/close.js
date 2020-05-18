8873dd3f12de6d971a411dbde965c30944691da3
Алексей Стребулаев <asmir800@mail.ru>
2020-05-18 08:48:34 +0300

    Готовый проект

--- source/js/close.js
+++
@@ -1,7 +0,0 @@
-var close = popup.querySelector(".button-close");
-
-close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("popup-show");
-  popup.classList.remove("popup-error");
-});
